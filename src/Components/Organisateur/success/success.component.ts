fef8155dc9ca8c9a1d2b1e4a290dcf2cbcd40e1b
Raania1 <boujnehrania3@gmail.com>
2025-06-07 15:46:22 +0100

    update

--- src/Components/Organisateur/success/success.component.ts
+++ src/Components/Organisateur/success/success.component.ts
@@ -3,53 +3,14 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PaiementService } from '../../../Services/paiement.service';
 import { CommonModule } from '@angular/common';
 import { ContractService } from '../../../Services/contrat.service';
+import { NavbarORComponent } from '../navbar-or/navbar-or.component';
 
 @Component({
   selector: 'app-success',
-  standalone: true,
-  imports: [CommonModule,RouterLink],
-  template: `
-<div class="flex items-center justify-center min-h-screen p-4">
-  <div class="max-w-md w-full mx-auto border border-custom-gray/30 rounded-lg overflow-hidden">
-    <div class="bg-green-100 border-b border-custom-gray/30 p-6">
-      <h2 class="text-center text-xl font-serif font打了ockquotebold text-green-700">Paiement réussi</h2>
-    </div>
-    <div class="text-center p-6 bg-white">
-      <div class="flex justify-center mb-6">
-        <div class="rounded-full bg-green-100 p-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-10 w-10 text-green-700"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-          </svg>
-        </div>
-      </div>
-      <p *ngIf="message" class="mb-4 text-gray-600">{{ message }}</p>
-      <button
-        *ngIf="contractCreated"
-        (click)="downloadContract()"
-        class="mt-4 border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white py-2 px-4 rounded-md"
-      >
-        Télécharger le contrat
-      </button>
-    </div>
-    <div class="bg-green-50 border-t border-custom-gray/30 p-6">
-      <button 
-        routerLink="/homeOr"
-        class="w-full border border-green-500 text-green-500 hover:bg-green-500 hover:text-white py-2 px-4 rounded-md"
-      >
-        Retour à la page d'accueil
-      </button>
-    </div>
-  </div>
-</div>
-`
-
-
+  standalone: true, 
+  imports: [CommonModule,RouterLink,NavbarORComponent],
+  templateUrl:'./success.component.html',
+  styleUrl: './success.component.css'
 })
 export class SuccessComponent {
   message: string = '';
